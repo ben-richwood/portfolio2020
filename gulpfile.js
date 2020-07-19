cd7d6264e810f68b204c427130097daf43e4757d
Ben <benjamin@bliss-interactive.com>
2020-07-19 10:04:26 +0700

    Img pipeline: Prepare the image pipiline, from jpg/png to optimized jpg, jp2 and webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 var path = require('path');
 const gulp = require('gulp');
+
 const webp = require('gulp-webp');
 var gulpCopy = require('gulp-copy');
+var rename = require("gulp-rename");
+var imageResize = require('gulp-image-resize');
+const imagemin = require('gulp-imagemin');
+var newer = require('gulp-newer');
 
 var sass = require('gulp-sass');
 
 var svgstore = require('gulp-svgstore');
 var svgmin = require('gulp-svgmin');
 var inject = require('gulp-inject');
+
+var imgSrc = 'src/assets/img/projects/**';
+var imgDest = 'dist/assets/img/all-projects';
+
 
 gulp.task('svgstore', function () {
   return gulp
@@ -27,13 +36,79 @@ gulp.task('svgstore', function () {
     .pipe(gulp.dest('dist/assets/img'));
 });
 
+// image-resize
+gulp.task('image-resize', function () {
+  gulp.src('test.png')
+    .pipe(imageResize({
+      width : 1000,
+      // height : 100,
+      // crop : true,
+      upscale : false,
+      quality: 0.8,
+      format: "jpg",
+      imageMagick: true
+    }))
+    .pipe(gulp.dest(imgDest));
+});
+
+// imagemin
+gulp.task('imgmin', () =>
+  gulp.src(['src/assets/img/projects/**/*.jpg', 'src/assets/img/projects/**/*.png'])
+    .pipe(newer(imgDest))
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.mozjpeg({quality: 75, progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      ]))
+    .pipe(gulp.dest(imgDest))
+);
+
 
 gulp.task('img', () =>
-  gulp.src('src/assets/img/projects/**/*.jpg')
-    .pipe(gulpCopy('dist/assets/img/all-projects', { prefix: 4 }))
-    .pipe(webp())
+  gulp.src(['src/assets/img/projects/**/*.jpg', 'src/assets/img/projects/**/*.png'])
+    // .pipe(gulpCopy('dist/assets/img/all-projects', { prefix: 4 }))
+    // .pipe(newer(imgDest))
+    .pipe(imageResize({
+      width : 1000,
+      // height : 100,
+      // crop : true,
+      upscale : false,
+      quality: 0.8,
+      format: "jpg",
+      imageMagick: true
+    }))
+    // .pipe(webp())
     .pipe(gulp.dest('dist/assets/img/all-projects'))
 );
+
+gulp.task('img-mobile', () =>
+  gulp.src(['src/assets/img/projects/**/*.jpg', 'src/assets/img/projects/**/*.png'])
+    // .pipe(gulpCopy('dist/assets/img/all-projects', { prefix: 4 }))
+    // .pipe(newer(imgDest))
+    .pipe(imageResize({
+      width : 700,
+      // height : 100,
+      // crop : true,
+      upscale : false,
+      quality: 0.8,
+      format: "jpg",
+      imageMagick: true
+    }))
+    // .pipe(webp())
+    .pipe(rename(function (path) { path.basename += "-mobile"; }))
+    .pipe(gulp.dest('dist/assets/img/all-projects'))
+);
+gulp.task('webp', () =>
+  gulp.src(['src/assets/img/projects/**/*.jpg', 'src/assets/img/projects/**/*.png'])
+    // .pipe(newer(imgDest))
+    .pipe(webp({
+      quality: 70,
+      metadata: ["icc"]
+    }))
+    .pipe(gulp.dest(imgDest))
+);
+
+gulp.task('img-pipeline', gulp.series('img', 'img-mobile', 'webp'))
 
 gulp.task('sass', function () {
  return gulp.src('./src/assets/scss/main.scss')
